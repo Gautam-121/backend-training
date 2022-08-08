622cb89e96f3a2189d26e8054dc50c3c5083e67c
Gautam <gautamdhakate1234@gmail.com>
2022-08-08 13:52:54 +0530

    using lodash package add router test-me at route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require("lodash")
 const loggerHandler = require("../logger/logger")
 const {printDate, printMonth, getBatchInfo} = require("../util/util")
 const {trim , lowerCase , upperCase} = require("../validator/formatter")
@@ -13,10 +14,34 @@ router.get("/test-function",(req,res)=>{
         trim();
         lowerCase();
         upperCase();
-}
-)
 
-router.get('/give-me-students-data',function(req, res){
+})
+
+
+router.get('/test-me',function(req, res){
+    let monthOfYear = ["jan","feb","mar","apr","may","jun","jul","aug","sept","oct","nov","dec"]
+    let createFourEqualPart = lodash.chunk(monthOfYear,3)
+    console.log(createFourEqualPart)
+
+    let firstTenOddNumber = [1,3,5,7,9,11,13,15,17,19]
+    let lastNineOddNumber = lodash.tail(firstTenOddNumber)
+    console.log(lastNineOddNumber)
+
+    let firstArray = [1,2,3]
+    let secondArray = [2,3,4]
+    let thirdArray = [3,4,5]
+    let fourthArray = [4,5,6]
+    let fifthArray = [5,6,7]
+
+    let unionOfAllTheArray = lodash.union(firstArray,secondArray,thirdArray,fourthArray,fifthArray)
+    console.log(unionOfAllTheArray)
+    
+    let movieAndDrama = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    let pairOfmovieAndDrama = lodash.fromPairs(movieAndDrama)
+    console.log(pairOfmovieAndDrama)
+
+    res.send("My first Api")
+
 
 })
 module.exports = router;
