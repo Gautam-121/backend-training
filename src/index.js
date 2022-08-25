e2cceb8b7f137fdf68e3ac3d8c5816884a8cb169
Gautam <gautamdhakate1234@gmail.com>
2022-08-25 21:27:37 +0530

    applicattion level middleWare

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const url = require("url")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,10 +16,27 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .catch ( err => console.log(err) )
 
 app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
+         
+    function(req,res,next){
+        
+        const newDate = new Date()
+        const ip = req.ip
+        const routerName =  req.url
+        
+
+        console.log(newDate , ip ,routerName)
+
+        next()
+
+
+
+//         e.g: you should be logging something like this on each line:
+// time , IP, Route should be printed on each line in terminal( every time an api is hit)
+// 2010-08-19 14:00:00 , 123.459.898.734 , /createUser
+// 2010-08-19 14:00:00 , 123.459.898.734 , /basicAPi
+// 2010-08-19 14:00:00 , 123.459.898.734 , /falanaAPI
+    }
+
   );
 
 app.use('/', route);
